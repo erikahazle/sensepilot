adcb054ba0e6c2ae4bdcebc72bff56c8d5ac6b11
erikajon <hello@jonika.io>
2024-05-21 21:02:50 +0100

    Fixed form

--- components/Contact/NewsLatterBox.tsx
+++ components/Contact/NewsLatterBox.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { useState } from "react";
 
 const NewsLatterBox = () => {
@@ -59,14 +61,17 @@ const NewsLatterBox = () => {
                 <input
                   type="email"
                   name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.currentTarget.value)}
                   placeholder="Enter your email"
                   className="mb-4 w-full rounded-md border border-body-color border-opacity-10 py-3 px-6 text-base font-medium text-body-color placeholder-body-color outline-none focus:border-primary focus:border-opacity-100 focus-visible:shadow-none dark:border-white dark:border-opacity-10 dark:bg-[#242B51] focus:dark:border-opacity-50"
                 />
                 <button
                   onClick={handleSubmit}
-                  value="Join us"
                   className="duration-80 mb-4 w-full cursor-pointer rounded-md border border-transparent bg-primary py-3 px-6 text-center text-base font-medium text-white outline-none transition ease-in-out hover:bg-opacity-80 hover:shadow-signUp focus-visible:shadow-none"
-                />
+                >
+                  Join us
+                </button>
                 {submitMessage && (
                   <p className="text-center text-base font-medium leading-relaxed text-body-color">
                     {submitMessage}
