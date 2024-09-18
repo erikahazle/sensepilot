17dbba929f642e104ac5ad2016f5af6dc6b5a882
erikajon <hello@jonika.io>
2024-09-18 19:41:28 +0100

    added links to policies

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -129,7 +129,7 @@ const Footer = () => {
               </div>
             </div> */}
 
-            {/* <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
+            <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
               <div className="mb-12 lg:mb-16">
                 <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
                   Terms
@@ -137,16 +137,20 @@ const Footer = () => {
                 <ul>
                   <li>
                     <a
-                      href="/"
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://app.termly.io/policy-viewer/policy.html?policyUUID=adc5508e-3f8c-40b0-86f8-ce0fc5e4bb3a"
                       className="mb-4 inline-block text-base font-medium text-body-color hover:text-primary"
                     >
                       {" "}
-                      TOS{" "}
+                      Terms and Conditions{" "}
                     </a>
                   </li>
                   <li>
                     <a
-                      href="/"
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://app.termly.io/policy-viewer/policy.html?policyUUID=a979fea0-7309-4128-9014-fe5ed8ec5409"
                       className="mb-4 inline-block text-base font-medium text-body-color hover:text-primary"
                     >
                       {" "}
@@ -155,16 +159,18 @@ const Footer = () => {
                   </li>
                   <li>
                     <a
-                      href="/"
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://app.termly.io/policy-viewer/policy.html?policyUUID=8e269881-0441-48ea-824b-c68a732183b1"
                       className="mb-4 inline-block text-base font-medium text-body-color hover:text-primary"
                     >
                       {" "}
-                      Refund Policy{" "}
+                      Cookie Policy{" "}
                     </a>
                   </li>
                 </ul>
               </div>
-            </div> */}
+            </div>
 
             {/* <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-3/12">
               <div className="mb-12 lg:mb-16">
